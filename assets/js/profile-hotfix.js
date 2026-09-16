282754918e63da6dac68c11eef2fa7348d6e0d0f
Sofia Kirsanova <23613069+SofiaKirsanova@users.noreply.github.com>
2026-09-16 18:36:58 -0500

    Update profile-hotfix.js

--- assets/js/profile-hotfix.js
+++ assets/js/profile-hotfix.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
   $('img[alt="DARPA logo"]').attr('src',darpaLogo);
 
   if(path.indexOf('/cv/')===0){
-    $('.post-description a').filter(function(){return $(this).text().indexOf('Industry Resume')!==-1;}).attr('href','/assets/pdf/CV_FAANG.pdf');
+    $('.post-description a').filter(function(){return $(this).text().indexOf('Industry Resume')!==-1;}).attr('href','/assets/pdf/kirsanova_resume.pdf');
   }
 });
